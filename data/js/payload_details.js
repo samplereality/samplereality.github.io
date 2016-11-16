cf6a01f62c6a29f4a2d7bf5b6c645c5bac4834a7
Mark Sample <samplereality@gmail.com>
2016-11-16 09:31:22 -0500

    Added 16 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
-var payload_details =  {
-  "tweets" : 37677,
-  "created_at" : "2016-11-15 17:07:17 +0000",
-  "lang" : "en"
+var payload_details = {
+	"tweets": 37693,
+	"created_at": "Wed Nov 16 14:31:22 +0000 2016",
+	"lang": "en"
 }
