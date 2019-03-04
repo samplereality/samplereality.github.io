24188de91729f5d7e8f752793ec2e937c7de6924
Mark Sample <samplereality@gmail.com>
2019-03-04 02:17:34 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 44143,
-	"created_at": "Sun Mar 03 07:17:33 +0000 2019",
+	"tweets": 44144,
+	"created_at": "Mon Mar 04 07:17:34 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Mon Mar 04 02:01:20 +0000 2019",
+		"id": 1102308044187287600,
+		"id_str": "1102388524706353152",
+		"full_text": "People who claim \"boys are easy\" always raise assholes.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 984457433518166000,
+			"id_str": "984457433518166016",
+			"name": "X-Men Origins: Storm is over due!",
+			"screen_name": "AliciaBLike",
+			"location": "in Trevante's dreams",
+			"description": "She/her. #AllBlackLivesMatter Abolish police & prisons. Decriminalize weed & sex work. #DeportTheColonizers",
+			"url": "https://t.co/XoTMlmjfSt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XoTMlmjfSt",
+							"expanded_url": "http://cash.me/$aliciafia",
+							"display_url": "cash.me/$aliciafia",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3428,
+			"friends_count": 576,
+			"listed_count": 115,
+			"created_at": "Thu Apr 12 15:45:17 +0000 2018",
+			"favourites_count": 19892,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 135420,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/997579060262916096/dLJ9NCSf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/997579060262916096/dLJ9NCSf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/984457433518166016/1547361860",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 392,
+		"favorite_count": 1504,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "People who claim \"boys are easy\" always raise assholes."
+	},
+	{
 		"created_at": "Sat Mar 02 12:23:50 +0000 2019",
 		"id": 1101820406674149400,
 		"id_str": "1101820406674149378",
